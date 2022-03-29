5d756c613d40ab6b0a546f09e85dd4cd3554426c
kroneldo28 <kroneldo28@yahoo.fr>
2022-03-29 23:11:21 +0100

    Played with buttons and entries

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 from tkinter import *
 import playground
 
-window = Tk()
-window.title("My First GUI program ")
-window.minsize(width=500, height=300)
 
-# Creating a label
-my_label = Label(text="My First Label", font=("Arial", 24, "bold"))
-my_label.pack()
-
-
-# Creating a button
 def button_clicked():
     # my_label.config(text="Button got clicked")
     my_label.config(text=my_input.get())
     print("I got clicked")
 
 
+window = Tk()
+window.title("My First GUI program ")
+window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
+
+# Creating a label
+my_label = Label(text="My First Label", font=("Arial", 24, "bold"))
+# my_label.pack()
+# my_label.place(x=0, y=0)
+my_label.grid(column=0, row=0)
+
+# Creating buttons
 my_button = Button(text="Click me", command=button_clicked)
-my_button.pack()
+# my_button.pack()
+my_button.grid(column=1, row=1)
+second_button = Button(text="Or Me")
+second_button.grid(column=2, row=0)
+
 
 # Creating an Entry (Input)
 my_input = Entry(width=10)
-my_input.pack()
-# new_label = my_input.get()
+# my_input.pack()
+my_input.grid(column=3, row=2)
 
 
-
-print(playground.add(1, 2, 3, 4, 5, 6))
+# print(playground.add(1, 2, 3, 4, 5, 6))
 
 window.mainloop()
